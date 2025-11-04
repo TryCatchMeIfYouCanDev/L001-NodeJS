4dad151f5620b8e24530b6e1bc45a575e4cbc7c6
Marco Fuchs <furfaro.markus@gmail.com>
2025-11-04 21:36:27 +0100

    Initial Commit

---
+++ node-basic/src/app.ts
@@ -0,0 +1,9 @@
+let s: string = "pippo";
+console.log(s);
+
+/** File proveniente da index.js a scopo didattico */
+let frutti: string []  = ["mela", "pera", "banana"];
+
+for (const frutto of frutti) {
+  console.log(frutto);
+}
